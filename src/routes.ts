b0598e506ab1c83dce74b128bfded09cb536eda1
naokiota1 <naokigno@gmail.com>
2023-09-28 08:52:28 -0300

    projeto

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
+const router = Router();
+
+// -- ROTAS USER --
+router.post("/users", new CreateUserController().handle);
+
+export { router };
